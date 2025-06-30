8a5982a3a8d1fee87784ee7444823f0cee2437c7
AnthoBean <anthony@mlicko.me>
2025-06-29 20:04:45 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+
+📡 Power Price Monitor
+-----------------------
+Now:   {{ IDX_0.current_price }}¢/kWh {{ IDX_0.current_status }}
+Next:  {{ IDX_0.next_price }}¢/kWh {{ IDX_0.next_status }}
+-----------------------
+Updated: {{ IDX_0.timestamp }}
